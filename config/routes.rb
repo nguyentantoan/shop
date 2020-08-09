2c6ccc2a0ec7dd8a129c1edafeb8dc913a1f6703
tantoan <nguyentantoan999@gmail.com>
2020-08-09 16:18:05 +0700

    signup

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
+  root "welcome#index"
+  get 'users/sign_up' => 'users#new'
+  post 'users/sign_up' => 'users#create'
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
